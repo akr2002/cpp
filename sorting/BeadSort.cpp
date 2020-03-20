91d9ba141b2780f46c1996f4804985d95cbe01e1
Aditya <adityakr082@gmail.com>
2020-03-20 17:25:17 +0530

    finished BeadSort.cpp

--- sorting/BeadSort.cpp
+++ sorting/BeadSort.cpp
@@ -15,5 +15,49 @@ void beadSort(int *a, int len)
 
 	// allocating memory
 	unsigned char beads[max*len];
-	memset(beads, 0, sizeof(
+	memset(beads, 0, sizeof(beads));
+
+	// mark the beads
+	for (int i = 0; i < len; ++i)
+		for (int j = 0; j < a[i]; ++j)
+			BEAD(i, j) = 1;
+
+	for (int j = 0; j < max; ++j)
+	{
+		// count how many beads are on each post
+		int sum = 0;
+		for (int i = 0; i < len; ++i)
+		{
+			sum += BEAD(i, j);
+			BEAD(i, j) = 0;
+		}
+
+		// Move beads down
+		for (int i = len - sum; i < len; ++i)
+			BEAD(i, j) = 1;
+	}
+
+	// Put sorted values in array using beads
+	for (int i = 0; i < len; ++i)
+	{
+		int j;
+		for (j = 0; j < max && BEAD(i, j); ++j);
+
+		a[i] = j;
+	}
 }
+
+// driver function to test the algorithm
+int main()
+{
+	int a[] = {5, 3, 1, 7, 4, 1, 1, 20};
+	int len = sizeof(a)/sizeof(a[0]);
+
+	beadSort(a, len);
+
+	for (int i = 0; i < len; ++i)
+		printf("%d", a[i]);
+
+	return 0;
+}
+
