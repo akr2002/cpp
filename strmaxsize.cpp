09e49c9de6c1641e93fd06dca685f619e37d660b
Aditya <adityakr082@gmail.com>
2020-03-31 20:22:29 +0530

    std::stringmax_size

---
+++ strmaxsize.cpp
@@ -0,0 +1,16 @@
+// Returns the maximum length the string can reach
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str ("Test string");
+	std::cout << "size: " << str.size() << "\n";
+	std::cout << "length: " << str.length() << "\n";
+	std::cout << "capacity: " << str.capacity() << "\n";
+	std::cout << "max_size: " << str.max_size() << "\n";
+
+	return 0;
+}
+
