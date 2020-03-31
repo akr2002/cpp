6601051e57c9922bd12c6e96e5f4390f314e3eea
Aditya <adityakr082@gmail.com>
2020-03-31 20:17:56 +0530

    std::string::size

---
+++ strsize.cpp
@@ -0,0 +1,13 @@
+// Returns the length of the string, in terms of bytes
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str ("Test string");
+	std::cout << "The size of str is " << str.size() << " bytes.\n";
+
+	return 0;
+}
+
