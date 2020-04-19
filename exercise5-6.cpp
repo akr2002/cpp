bc3d4ea16c487cf0e5df8331f303353f1b357c5f
Aditya <adityar082@gmail.com>
2020-04-19 09:16:30 +0530

    initial commit

---
+++ exercise5-6.cpp
@@ -0,0 +1,4 @@
+// Exercise 5-6. Create a vector<> container with elements containing the 
+// integers from 1 to an arbitrary upper bound entered by the user. Output the 
+// elements from the vector that contains values taht are not multiples of 7 or
+// 13. Output them 10 on a line, aligned in columns.
