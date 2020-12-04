d6c184f1a13c3d7bafaa764ca9aefcb910af274f
GNUxeava <adityakr082@gmail.com>
2020-12-04 21:01:51 +0530

    removed binary

---
+++ arraySize.cpp
@@ -0,0 +1,23 @@
+/* Obtaining the number of array elements */
+
+#include <iostream>
+#include <array>
+
+int main()
+{
+	int values[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+	std::cout << "There are " << sizeof(values) / sizeof(values[0]) 
+		<< " elements in the array." << std::endl;
+
+	int sum {};
+	for (std::size_t i {}; i < std::size(values); ++i)
+	{
+		sum += values[i];
+	}
+
+	std::cout << "The sum of the array elements is " << sum << std::endl;
+
+	return 0;
+}
+
---
+++ mulTable.cpp
@@ -0,0 +1,61 @@
+/* Generating multiplication tables using nested loops */
+
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+
+int main()
+{
+	std::size_t table {};	// Table size
+	const std::size_t table_min {2};	// Minimum table size - at least up to the 2-times
+	const std::size_t table_max {12};	// Maximum table size
+	char reply {};	// Response to prompt
+
+	do
+	{
+		std::cout << "What size table would you like ("
+			<< table_min << " to " << table_max << ")? ";
+		std::cin >> table;	// Get the table size
+		std::cout << std::endl;
+
+		// Make sure table size is wihtin the limits
+		if (table < table_min || table > table_max)
+		{
+			std::cout << "Invalid table size entered. Program terminated." << std::endl;
+			return 1;
+		}
+
+		// Create the top line of the table
+		std::cout << std::setw(6) << " |";
+		for (std::size_t i {1}; i <= table; ++i)
+		{
+			std::cout << std::setw(3) << i << " |";
+		}
+		std::cout << std::endl;
+
+		// Create the separator row
+		for (std::size_t i {1}; i <= table; ++i)
+		{
+			std::cout << "------";
+		}
+		std::cout << std::endl;
+
+		for (std::size_t i {1}; i <= table; ++i)
+		{	// Iterate over rows
+			std::cout << " " << std::setw(3) << i << " |";	// Start the row
+			
+			// Output the values in a row
+			for (std::size_t j {1}; j <= table; ++j)
+			{
+				std::cout << " " << std::setw(3) << i * j << " |";	//For each col.
+			}
+			std::cout << std::endl;
+		}
+		
+		// Check if another table is required
+		std::cout << "\nDo you want another table (y or n)? ";
+		std::cin >> reply;
+	} while (std::tolower(reply) == 'y');
+
+	return 0;
+}
